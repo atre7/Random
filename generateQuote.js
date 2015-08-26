70fc2ae67cf708ea745b67bcfc74756e19b45220
atre7 <atrerc@gmail.com>
2015-08-26 19:13:07 +0200

    JSONP api zije
    cors

--- generateQuote.js
+++ generateQuote.js
@@ -1,9 +1,39 @@
 $(document).ready(function() {
 
+  // Using JSONP  CORS
+  $('#apiQuote').click(function() {
+    $.ajax({
+      url: "http://api.forismatic.com/api/1.0/",
+
+      // The name of the callback parameter, as specified by the YQL service
+      jsonp: "callback",
+
+      // Tell jQuery we're expecting JSONP
+      dataType: "jsonp",
+
+      // Tell YQL what we want and that we want JSON
+      data: "method=getQuote&format=jsonp&lang=en&jsonp=myCallback",
+
+      jsonpCallback: 'myCallback',
+
+
+      // Work with the response
+      success: function(response) {
+        console.log(response); // server response
+        $('#citation').text(response.quoteText);
+        $('#autor').text(response.quoteAuthor);
+      }
+
+    }, function(data) {
+      console.log("data " + data);
+    });
+
+  }); // apiQuote
 
   // ajax api quote
   // http://api.forismatic.com/api/1.0/?method=getQuote&format=text&lang=en
-  $('#apiQuote').click(function() {
+  // CORS Error
+  $('#apiQuoteVYRADENE').click(function() {
     var url = "http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en" ;
     $.get(url, function(data) {
       alert("recive quote: " + data.quoteText + " autor : " + data.quoteAuthor);
